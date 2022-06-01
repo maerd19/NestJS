ee27178a7adabd71d494343bdd9151ac7c8c5a3d
Alejandro Hernandez <revolutions_333@hotmail.com>
2022-06-01 13:13:50 -0500

    Connecting to MySQL using TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomersModule } from './customers/customers.module';
+import entities from './typeorm';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CustomersModule, UsersModule],
+  imports: [
+    CustomersModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'Pp$wW0rd',
+      database: 'tutorial_db',
+      entities,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
